0d393329f0cd0e57540f29a14a3a8e279f22fe2b
kimia <kimia.ehsani.dolati.al@iespoligonosur.org>
2022-04-25 19:12:19 +0200

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './services/authentication.service';
 
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AdopcionAnimales';
+
+  constructor(public _authService: AuthenticationService) { }
+
+  /**
+   * Borrar la información del usuario de la sesión para hacer un logout
+   */
+  public logout(){
+    sessionStorage.removeItem("nombreUsuario");
+    sessionStorage.removeItem("tipo");
+  }
+
+  
 }
