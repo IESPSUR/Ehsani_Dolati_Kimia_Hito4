310ead6f212dbeadf2d410be4887a04394f39b68
kimia <kimia.ehsani.dolati.al@iespoligonosur.org>
2022-04-26 02:06:29 +0200

    final commit

--- adoptAnimals/src/main/java/com/animals/DatabaseConfig.java
+++ adoptAnimals/src/main/java/com/animals/DatabaseConfig.java
@@ -106,8 +106,9 @@ public class DatabaseConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOrigins("http://localhost:4200")
-					.allowedMethods("GET", "POST", "PUT", "DELETE");
+					.allowedOrigins("*")
+					.allowedMethods("GET", "POST", "PUT", "DELETE")
+					.allowCredentials(true);
 			}
 			
 		};
